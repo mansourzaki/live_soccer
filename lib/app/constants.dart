dbee7d737d8096a67df3c7de10be81d353042325
Mansour Alhaddad <43302116+mansourzaki@users.noreply.github.com>
2022-09-08 19:14:53 +0300

    Update constants.dart

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,8 +1,8 @@
 class Constants {
   // static const String baseUrl = "https://v3.football.api-sports.io";
   static const String baseUrl = "https://api-football-v1.p.rapidapi.com/v3";
-  // static const String apiKey = "5a296043e82313dc021f7911240500dc";
-  static const String apiKey = "ac294982a5msh911bd7aae15b9e3p145274jsn25880bd9dabe";
+  // static const String apiKey = "";
+  static const String apiKey = "";
   static const int ZERO = 0;
   static const String EMPTY = "";
   static const String worldLogo =
